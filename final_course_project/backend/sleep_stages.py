0db8d313b0dab425bc85bc9478e721e2844f7636
Aylin <aylina.vch@gmail.com>
2024-10-30 09:05:41 -0300

    Updating gitignore

---
+++ final_course_project/backend/sleep_stages.py
@@ -0,0 +1,5 @@
+def correct_current_stage(current_stage, sleep_stages):
+    
+    #if sleep_stages[-1]=='R' and current_stage=='W' and len(sleep_stages)<30:
+        #current_stage = 'W'
+    return current_stage
